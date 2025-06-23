da476526bf685039b21d0d9bcacbc0322cbd87f7
hieu1966hn <hieu1966hn@gmail.com>
2025-06-23 09:46:02 +0700

    Initial commit

---
+++ SS1/SS1.py
@@ -0,0 +1,19 @@
+# print(1 + 1)
+# print(10)
+
+# input("Hôm nay ăn gì") # Câu lệnh nhập dữ liệu từ người dùng 
+
+print(f"8 x 1 = {8*1}") # f: dùng để truyền giá trị trong chuỗi
+print(f"8 x 2 = {8*2}") ## Cú pháp copy nhanh: shift + alt + xuống
+print(f"8 x 3 = {8*3}") 
+print(f"8 x 4 = {8*4}") 
+print(f"8 x 5 = {8*5}") 
+print(f"8 x 6 = {8*6}") 
+print(f"8 x 7 = {8*7}") 
+print(f"8 x 8 = {8*8}") 
+print(f"8 x 9 = {8*9}") 
+print(f"8 x 10 = {8*10}") 
+
+
+## code thường
+print(f"8 x 1 = {8*1}") 
